a7cccf4e7c22ca08446f785ed1f82c1192a32151
Jimmy <zinuhe@outlook.com>
2021-12-07 00:34:47 -0500

    Added queue

--- queue/main.go
+++ queue/main.go
@@ -1,13 +1,49 @@
 package main
 
 import (
-         "fmt"
+	"fmt"
 )
 
-//Enqueue
+type Queue []string
 
+func (q *Queue) enQueue(value string) {
+    *q = append(*q, value)
+}
 
-//Dequeue
+func (q *Queue) deQueue() (string, bool) {
+    if len(*q) > 0 {
+        result := (*q)[0]
+        *q = (*q)[1:]
+        return result, false
+    }
+    return "", true
+}
+
+func (q *Queue) print() {
+    fmt.Println("queue:", *q)
+}
 
 func main() {
+    var queue Queue
+    queue.print()
+    queue.enQueue("one")
+    queue.print()
+    queue.enQueue("two")
+    queue.print()
+    queue.enQueue("three")
+    queue.print()
+
+    if res, err := queue.deQueue(); err == false {
+        fmt.Println("Value from Queue:", res)
+        queue.print()
+    } else {
+        fmt.Println("No more values in the Queue")
+    }
+
+    if res, err := queue.deQueue(); err == false {
+        fmt.Println("Value from Queue:", res)
+        queue.print()
+    } else {
+        fmt.Println("No more values in the Queue")
+    }
 }
